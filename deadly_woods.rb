dc3d3d3c4598241d43fa78619a39b83d132986dd
locks <rokusu@gmail.com>
2010-10-22 21:21:11 +0100

    renamed main file

---
+++ deadly_woods.rb
@@ -0,0 +1,68 @@
+require 'pp'
+require './lib/scene.rb'
+require './lib/world.rb'
+
+game = World.new("--Scott Thomas and the Revolving Door--")
+
+game.add_scene(
+  :introduction,
+  "Welcome to the game.
+A short Description follows.",
+  {
+    :frog  => "You kill a frog.\t\t\t",
+    :key   => "You see a key on the floor.\t\t",
+    :north => "There is a road leading to some woods."
+  },
+  {
+    :look_at => { :frog => 'You see a dead frog.' },
+    :pick_up => {
+      :key  => {
+        :description => 'You pick the key.',
+        :modifier => lambda{|scene|
+          scene.options.store(:key, "The key feels heavy in your hand.\t")
+          scene.actions[:use][:key][:picked] = true
+        }
+      }
+    },
+    :go => {
+      :north => {
+        :description => "You trod towards the woods",
+        :target      => :woods
+      }
+    },
+    :use => {
+      :key => {
+        :description => "A magic gate unfolds before your eyes.",
+        :modifier => lambda do |scn|
+          scn.options.store(:south, "The magic gate looms\t\t\t")
+          scn.actions[:go].store(:south, {
+              :description => "You enter through the magic ",
+              :target      => :woods
+          })
+        end,
+        :picked => false
+      }
+    }
+  }
+)
+
+game.add_scene(
+  :woods,
+  "You are at the woods. Finally.
+
+  ... You die.",
+  {
+    :past => "Go past in time.",
+    :exit => "Exit the game."
+  },
+  {
+    :go => {
+      :past => {
+        :description => "Death slowly turns back your hourglass.",
+        :target      => :introduction
+      }
+    }
+  }
+)
+
+game.start
