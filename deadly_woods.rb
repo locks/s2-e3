063e7ac748ae503581d48c293147c148a49aef02
locks <rokusu@gmail.com>
2010-10-23 00:20:12 +0100

    New DSL syntax plus addition of second scene to DSLed game.

--- deadly_woods.rb
+++ deadly_woods.rb
@@ -13,23 +13,33 @@ game.bladd_scene(:introduction) do
   option :north, "There is a road leading to some woods"
 
   action :look_at, :frog, 'You see a dead frog.'
-  action :pick_up, :key,  'You pick the key', lambda {|scn|
+  action :pick_up, :key,  'You pick the key.' do |scn|
         scn.options.store :key, "The key feels heavy in your hand.\t"
         scn.actions[:use][:key][:picked] = true
-  }
-  action :go,  :north, 'You trod towards the woods.', :woods
-  action :use, :key,   "A magic gate unfolds before your eyes.", lambda {|scn|
+  end
+  action :go, :north, 'You trod towards the woods.', :woods
+  action :use, :key,   "A magic gate unfolds before your eyes." do |scn|
       scn.options.store :south, "The magic gate looms...\t\t\t"
       scn.actions[:go].store :south, {
         :description => "You enter through the magic gate.",
         :target      => :woods
       }
-  }
+  end
   action :use, :frog, "You make a lucky charm for your key ring. Ew ew ew.", lambda {|scn| scn.options.delete :frog}
+end
 
-end 
+game.bladd_scene(:woods) do
+  
+  description "You are at the woods. Finally.\n\n  ... You die."
+  
+  option :back, "Go back in time."
+  option :exit, "Retire to aetherland."
+  
+  action :go, :back, "Death carefully inverts your hourglass for a couple of seconds.", :introduction
+end
 
-game.add_scene(
+old_game = World.new("Another One")
+old_game.add_scene(
   :introduction,
   "Welcome to the game.
 A short Description follows.",
@@ -76,7 +86,7 @@ A short Description follows.",
   }
 )
 
-game.add_scene(
+old_game.add_scene(
   :woods,
   "You are at the woods. Finally.
 
@@ -94,5 +104,11 @@ game.add_scene(
     }
   }
 )
+
+pp game
+puts
+pp old_game
+
+
 
 game.start
