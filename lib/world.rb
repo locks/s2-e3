93186cc558999a261228bbfeebf9cffa0129fc95
locks <rokusu@gmail.com>
2010-10-25 18:24:25 +0100

    Refactor trying to implement Greg's suggestions.

--- lib/world.rb
+++ lib/world.rb
@@ -1,26 +1,23 @@
 class World
-  attr_accessor :title, :description, :scenes
+  attr_accessor :title, :description, :scenes, :output
     
-  def initialize(title="Generic Title", description="Generic Description", scenes=[])
-    @title       = title
-    @description = description
-    @scenes      = scenes
+  def initialize(title="Generic Title", description="Generic Description", scenes=[], output=STDOUT)
+    @title, @description, @scenes = title, description, scenes
+    
+    @output = output
   end
   
-  def add_scene(*args)
-    @scenes << Scene.new(*args)
-  end
-  
-  def dsl_add_scene(key, &block)
+  def add_scene(key, &block)
     scn = Scene.new(key)
-    scn.instance_eval &block
+    scn.instance_eval(&block)
+    scn.output = @output
     
     @scenes << scn
   end
   
   def start
-    puts "--#{@title}--", ""
-    puts @description, ""
+    say("--#{@title}--", "")
+    say(@description, "")
     
     switch_scene :introduction
     parse_command
@@ -31,38 +28,38 @@ class World
       scn.id==scene and @current_scene = scn
     end
   end
-  
-  def go(direction)
-    switch_scene @current_scene.go(direction)
-  end
 
   def parse_command
-  while true
-    @current_scene.read_scene
+    while true
+      @current_scene.read_scene
   
-    print "> "
-    input = gets.to_s.strip.downcase.split(" ")
+      print "> "
+      input = gets.to_s.strip.downcase.split(" ")
   
-    if input.empty?
-      puts "Sorry, I don't understand that",""
-      next
-    end
+      if input.empty?
+        say("Sorry, I don't understand that","")
+        next
+      end
 
-    if input[0] == 'exit'
-      puts "Thank you for playing.", "Exiting... or am I...?"
-      exit
+      if input[0] == 'exit'
+        say("Thank you for playing.", "Exiting... or am I...?")
+        exit
+      end
+  
+      if input.size > 2
+        input = [input[0..1].join("_")] + input[2..-1]
+      end
+
+      if input[0] == 'go'
+        switch_scene @current_scene.go(input[1])
+      else
+        @current_scene.send input[0], input[1]
+      end
     end
-  
-    if input.size > 2
-      input = [input[0..1].join("_")] + input[2..-1]
-    end
-  
-    self.send input[0].to_sym, input[1]
-  end
   end
   
-  def method_missing(symbol, *args)
-    @current_scene.send symbol, *args
+  def say(*msg)
+    @output.puts(msg)
   end
 
 end
